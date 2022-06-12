cd991005703d66e390470727ae1679002b62699d
cihanandac <cihanandac@gmail.com>
2022-06-13 01:21:20 +0300

    Added button functions for 'New Canvas', 'Change Pen Color', and 'Change Background Color' buttons.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const container = document.querySelector('.container');
 
+let chosenPenColor = 'black';
+
+
 for(let i=0; i<64; i++){
   const row = document.createElement('div');
   row.setAttribute('id', 'row');
@@ -18,7 +21,7 @@ squares.forEach(selectSquare => selectSquare.addEventListener('mouseenter', draw
 
 function drawing() {
   if (mouseDown) {
-    this.style.backgroundColor = 'black'}
+    this.style.backgroundColor = chosenPenColor}
 } 
 
 var mouseDown = 0;
@@ -28,3 +31,19 @@ document.body.onmousedown = function() {
 document.body.onmouseup = function() {
   --mouseDown;
 }
+
+const cleanBtn = document.querySelector('#canvas');
+cleanBtn.addEventListener('click', ()=> {
+  squares.forEach(selectSquare => selectSquare.style.backgroundColor = 'white');
+})
+
+const penColor = document.querySelectorAll('.penColor');
+penColor.forEach(pen => pen.addEventListener('click', ()=>{
+  chosenPenColor = pen.id;
+}))
+
+const bgColor = document.querySelectorAll('.bgColor');
+bgColor.forEach(bg => bg.addEventListener('click', ()=>{
+  squares.forEach( 
+    selectSquare => selectSquare.style.backgroundColor = bg.id
+    )}))
