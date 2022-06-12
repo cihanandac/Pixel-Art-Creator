21f47b5af611203e054d5c0cb2b82270348db93d
cihanandac <cihanandac@gmail.com>
2022-06-12 23:38:41 +0300

    Fixed clicked and hold event listener

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const container = document.querySelector('.container');
 
-for(let i=0; i<16; i++){
+for(let i=0; i<64; i++){
   const row = document.createElement('div');
+  row.setAttribute('id', 'row');
   container.appendChild(row);
-  for(let i=0; i<16; i++){
+  for(let i=0; i<64; i++){
     const square = document.createElement('span');
     square.className = 'square';
     row.appendChild(square);
-    square.innerHTML = 's';
+    square.innerHTML = '';
   } 
 }
 
 
 const squares = document.querySelectorAll('.square');
-squares.forEach(selectSquare => selectSquare.addEventListener('mousedown', (e) => {
-  selectSquare.style.backgroundColor = "black";  
-}));
+squares.forEach(selectSquare => selectSquare.addEventListener('mouseenter', drawing));
+
+function drawing() {
+  if (mouseDown) {
+    this.style.backgroundColor = 'black'}
+} 
+
+var mouseDown = 0;
+document.body.onmousedown = function() { 
+  ++mouseDown;
+}
+document.body.onmouseup = function() {
+  --mouseDown;
+}
